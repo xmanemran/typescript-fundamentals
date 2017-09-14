9c8dec66cf6c1bc2a0af4f04051bd8bfc57fd6e9
Michael North <michael.l.north@gmail.com>
2017-09-14 01:06:00 -0500

    stack solution

--- exercises/stack/src/stack.ts
+++ exercises/stack/src/stack.ts
@@ -6,6 +6,46 @@ interface IStack<T> {
   print(): void;
 }
 
-export class Stack<T> {
+interface ListNode<T> {
+  data: T;
+  next?: ListNode<T>;
+}
 
+export class Stack<T> implements IStack<T> {
+  private head: ListNode<T> | undefined
+  push(item: T): Stack<T>
+  push(items: T[]): Stack<T>
+  push(arg: T[] | T) {
+    if (arg instanceof Array) {
+      arg.forEach(item => this.push(item));
+    } else {
+      let newNode = {
+        data: arg,
+        next: this.head
+      }
+      this.head = newNode;
+    }
+    return this;
+  }
+  pop() {
+    let n = this.head;
+    this.head = this.head ? this.head.next : undefined;
+    return n ? n.data : undefined;
+  }
+  length() {
+    let i = 0;
+    let n: ListNode<T> | undefined = this.head;
+    while(n) {
+      i++;
+      n = n.next;
+    }
+    return i;
+  }
+  print() {
+    let n: ListNode<T> | undefined = this.head;
+    while(n) {
+      console.log(n.data);
+      n = n.next;
+    }
+  }
 }
