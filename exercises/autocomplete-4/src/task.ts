918a304bc91929f5abcfbd45c695374d041ad211
Michael North <michael.l.north@gmail.com>
2017-09-15 01:39:25 -0500

    spawn function

--- exercises/autocomplete-4/src/task.ts
+++ exercises/autocomplete-4/src/task.ts
@@ -9,7 +9,23 @@ import { isPromise } from './utils/promise';
 export function task<T>(genFn: () => IterableIterator<any>): Promise<T> {
   let p = new Promise<T>((resolve) => {
     let iterator = genFn(); // Get the iterator
-    // TODO: implement your solution here
+    let lastValue: any = null;
+    keepGoing();
+    function keepGoing(arg?: any) {
+
+      let yielded = iterator.next.call(iterator, arg);
+      if (isPromise(yielded.value)) {
+        yielded.value.then((d: any) => {
+          lastValue = yielded.value;
+          keepGoing(d);
+        });
+      } else if (!yielded.done) {
+        lastValue = yielded.value;
+        keepGoing(yielded.value);
+      } else {
+        resolve(lastValue);
+      }
+    }
   });
   return p;
 }
