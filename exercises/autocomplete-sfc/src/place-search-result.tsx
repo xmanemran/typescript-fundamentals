ff6f1af4d2ce3bb9e7e2299944e46243a02b43b2
Michael North <michael.l.north@gmail.com>
2017-09-15 00:16:24 -0500

    React SFC solution

--- exercises/autocomplete-sfc/src/place-search-result.tsx
+++ exercises/autocomplete-sfc/src/place-search-result.tsx
@@ -1,6 +1,21 @@
 import * as React from 'react';
-export const PlaceSearchResult: React.SFC<any> = () => {
+import { PlaceDetails } from './utils/places';
+import { shortUrl } from './utils/string';
+
+
+export const PlaceSearchResult: React.SFC<PlaceDetails> = (placeInfo) => {
+  let url = placeInfo.website ? <a href={placeInfo.website} target="_blank">{shortUrl(placeInfo.website, 20)}</a> : '';
   return (
-    <div></div>
+    <li key={placeInfo.url} className="search-result">
+      <img className="icon" src={placeInfo.icon} />
+      <h3>{placeInfo.name}</h3>
+      <p>
+        <a href={placeInfo.url} target="_blank">{placeInfo.vicinity}</a>
+        {' '}
+        -
+        {' '}
+        {url}
+      </p>
+    </li>
   );
-};
+}
